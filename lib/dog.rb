fae2521cf3f279cd9b7c0d72e3813476ef7610ef
Ariel Zhen <zhenpingping@live.com>
2018-05-21 02:12:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
-  def name
-    @this_dogs_name
-  end
-
   def name=(dogs_name)
     @this_dogs_name = dogs_name
+  end
+
+  def name
+    @this_dogs_name
   end
 
   def breed
@@ -14,5 +14,5 @@ class Dog
 
   def breed=(dogs_breed)
     @this_dogs_breed
-  end 
+  end
 end
