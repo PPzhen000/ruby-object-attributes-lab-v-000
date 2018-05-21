ad726f0d79e3078f9b5d15b4e5b824f595f38b07
Ariel Zhen <zhenpingping@live.com>
2018-05-21 02:37:49 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,19 @@
 class Person
 
-  def initialize(name)
-    @name = name
-  end
-
   def name
     @name
   end
- 
-  def name=(new_name)
-    @name = new_name
+
+  def name=(persons_name)
+    @name = persons_name
   end
 
+  def job
+    @job
+  end
+
+  def job = (persons_job)
+    @job = persons_job
+  end
+  
 end
